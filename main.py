96b027bb722edc5224dbea6ceed18595aede7e51
lopezparralaurafernanda-dotcom <lopezparralaurafernanda@gmail.com>
2026-05-13 17:06:42 -0500

    MAIN

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Archivo orquestador del proyecto.
+
+Ejecuta este archivo para abrir la capa visual del MVP.
+"""
+
+from Frontend.interfaz import iniciar_app
+
+
+if __name__ == "__main__":
+    iniciar_app()
